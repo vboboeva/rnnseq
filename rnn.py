8c04907dac4c53649fe76de402ee9aaa1122c3b5
vboboeva <vezha.boboeva@gmail.com>
2023-07-10 18:10:54 +0100

    Adapted rnn file to new, correct sequences.

--- rnn.py
+++ rnn.py
@@ -13,10 +13,11 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from pprint import pprint
 from tqdm import tqdm
+import string
 
 
 class RNN(nn.Module):
-    def __init__(self, input_num_units, hidden_num_units, num_layers, output_num_units):
+    def __init__(self, input_num_units, hidden_num_units, num_layers, output_num_units, nonlinearity="tanh"):
         super(RNN, self).__init__()
 
         # Defining the number of layers and the nodes in each layer
@@ -24,7 +25,7 @@ class RNN(nn.Module):
         self.num_layers = num_layers
 
         # RNN layers
-        self.rnn = nn.RNN(input_num_units, hidden_num_units, num_layers)
+        self.rnn = nn.RNN(input_num_units, hidden_num_units, num_layers, nonlinearity=nonlinearity)
 
         # Fully connected layer
         self.fc = nn.Linear(hidden_num_units, output_num_units)
@@ -52,7 +53,7 @@ class RNN(nn.Module):
         # ht, hT = self.rnn(x, h0)
         ht, hT = self.rnn(x)
 
-        # whole sequence of hidden states, linarly transformed
+        # whole sequence of hidden states, linearly transformed
         y = self.fc(ht)
 
         return ht, hT, y
@@ -62,54 +63,58 @@ if __name__ == "__main__":
 
     # load the number of inputs
     alphabet = loadtxt('input/alphabet.txt', dtype='str')
-    print(alphabet)
     nb_classes=len(alphabet)
-    print(nb_classes)
+    
+    L=5
+    m=5
+
+    # make a dictionary
+    dicts = {}
+    keys = list(string.ascii_lowercase)[:nb_classes]
+    values = np.arange(nb_classes)
+    for i, k in enumerate(keys):
+        dicts[k] = values[i]
 
     # input_num_units, hidden_num_units, num_layers, output_num_units
-    model = RNN(nb_classes, 10, 1, 10)
+    model = RNN(nb_classes, 100, 1, 10)
 
-    print(model)
-
-    # for name, vals in model.rnn.named_parameters():
-    #     print(name, "-->", vals.shape)
-    #     print("-------------------------------")
-    #     print(vals)
-    #     print("\n")
-
-    # exit()
-
-    # x = torch.randn(5, 3, 10)
-    # ht, hT, output = model(x)
-
-    # print(ht[-1] - hT)
-
-    # print("x = ", x.shape)
-    # print("ht = ", ht.shape)
-    # print("output = ", output.shape)
+    
 
     optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0)
 
-    # cycles = 10
-    # x0 = torch.sin(torch.arange(50)/50*np.pi*2 * cycles)
-    # exit()
+    # load types
+    types = np.array(loadtxt('input/structures_L%d_m%d.txt'%(L, m), dtype='str')).reshape(-1)
 
+    all_tokens=[]
+    # load all the tokens corresponding to that type
+    for t, type_ in enumerate(types):
+        tokens = loadtxt('input/%s.txt'%type_, dtype='str')
+        tokens_arr = np.vstack([np.array(list(token_)) for token_ in tokens])
+        all_tokens.append(tokens_arr)
+    all_tokens = np.vstack(all_tokens)
+
+    
+    # count how many transitions of each kind we have
+    count=np.zeros((nb_classes, nb_classes))
+    for i in range(len(all_tokens)):
+        for j in range(L-1):
+            x = dicts[all_tokens[i,j]]
+            y = dicts[all_tokens[i,j+1]]
+            count[x,y]+=1
+    count /= np.sum(count, axis=1)[:,None]
+
+    # create train data
+    x = torch.zeros((L, len(all_tokens), nb_classes), dtype=torch.float32)
+    for i, token in enumerate(tokens):
+        pos = []
+        for letter in token:
+            pos = np.append(pos, dicts[letter])
+        x[:,i,:] = F.one_hot(torch.tensor(pos.astype(int)), nb_classes)
+    
+    # train network
     train_losses = []
-    for _ in tqdm(range(1000)):
-
-        # sequence of 50 i.i.d. Gaussian numbers
-        # x = torch.randn(50, 64, 10) #+ x0[:,None,None]
-        # x = F.softmax(x, dim=-1)
-
-        # load inputs
-        structure = loadtxt('input/structures.txt', dtype='str')[0]
-        print(structure)
-
-        one_hot_targets = np.eye(nb_classes)[targets]
-        print(one_hot_targets)
-
-        exit()
-
+    for _ in tqdm(range(100)):
+        
         optimizer.zero_grad()
 
         '''
@@ -125,34 +130,70 @@ if __name__ == "__main__":
 
         train_losses.append(loss.item())
 
+    Wio=np.dot( model.fc.state_dict()["weight"].detach().cpu().numpy(),
+         model.rnn.state_dict()["weight_ih_l0"].detach().cpu().numpy())
+    
+    fig, axs = plt.subplots(1,3, figsize=(14,4))
+    
+    axs[0].plot(train_losses)
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_ylabel('Training loss')
+    im1 = axs[1].imshow(Wio.T)#, vmin=0, vmax=1)
+    axs[1].set_xticks(np.arange(nb_classes))
+    axs[1].set_yticks(np.arange(nb_classes))
+    axs[1].set_xticklabels([string.ascii_lowercase[i] for i in range(nb_classes)])
+    axs[1].set_yticklabels([string.ascii_lowercase[i] for i in range(nb_classes)])
+
+    im2 = axs[2].imshow(count)#, vmin=0, vmax=1)
+    axs[2].set_xticks(np.arange(nb_classes))
+    axs[2].set_yticks(np.arange(nb_classes))
+    axs[2].set_xticklabels([string.ascii_lowercase[i] for i in range(nb_classes)])
+    axs[2].set_yticklabels([string.ascii_lowercase[i] for i in range(nb_classes)])
+
+    plt.colorbar(im1, ax=axs[1], fraction=0.046, pad=0.04)
+    plt.colorbar(im2, ax=axs[2], fraction=0.046, pad=0.04)
+    fig.savefig('loss.png')
 
 
+    '''
+    plot SVD decomposition of weights
+    Rows: 0 - input weights, 1 - recurrent weights, 2 - output weights
+    Cols: 0 - raw, 1 - singular values S, 2 - left singular vectors U, 3 - right singular vectors V
+    '''
 
+    fig, axs = plt.subplots(3,4, figsize=(13,8))
+    plt.tight_layout()
 
-    fig, ax = plt.subplots()
-    ax.plot(train_losses)
-    plt.show()
-    plt.close(fig)
+    axs[0,0].set_title("Weights")
+    axs[0,1].set_title("Singular values")
+    axs[0,2].set_title("Left singular vectors, U")
+    axs[0,3].set_title("Right singular vectors, V")
 
-    fig, ax = plt.subplots(2,2, figsize=(8,8))
+    def plot_svd (weights, title, axs):
+        axs[0].set_ylabel(title)
+        im = axs[0].imshow(weights)
+        plt.colorbar(im, ax=axs[0])
 
-    ax[0][0].set_title("weight . weight_ih_l0")
-    im = ax[0][0].imshow(np.dot(
-        model.fc.state_dict()["weight"].detach().cpu().numpy(),
-        model.rnn.state_dict()["weight_ih_l0"].detach().cpu().numpy()
-        ))
-    plt.colorbar(im, ax=ax[0][0])
+        U, S, Vh = np.linalg.svd(weights)
 
-    ax[0][1].set_title("weight_hh_l0")
-    im = ax[0][1].imshow(model.rnn.state_dict()["weight_hh_l0"].detach().cpu().numpy())
-    plt.colorbar(im, ax=ax[0][1])
+        # check that Uh.U and Vh.V are identity matrices
+        print(title)
+        # print("U = ", U.shape, np.linalg.norm(np.dot(U.T, U) -  np.eye(len(U)) ) )
+        # print("Vh = ", Vh.shape, np.linalg.norm(np.dot(Vh, Vh.T) -  np.eye(len(Vh)) ) )
 
-    ax[1,0].set_title("weight_ih_l0")
-    im = ax[1,0].imshow(model.rnn.state_dict()["weight_ih_l0"].detach().cpu().numpy())
-    plt.colorbar(im, ax=ax[1][0])
+        axs[1].plot(S)
+        im = axs[2].imshow(U)
+        plt.colorbar(im, ax=axs[2])
+        im = axs[3].imshow(Vh.T)
+        plt.colorbar(im, ax=axs[3])
 
-    ax[1,1].set_title("weight")
-    im = ax[1,1].imshow(model.fc.state_dict()["weight"].detach().cpu().numpy())
-    plt.colorbar(im, ax=ax[1][1])
-    plt.show()
-    plt.close(fig)
+    weights = model.rnn.state_dict()["weight_ih_l0"].detach().cpu().numpy()
+    plot_svd(weights, "weight_ih_l0", axs[0])
+
+    weights = model.rnn.state_dict()["weight_hh_l0"].detach().cpu().numpy()
+    plot_svd(weights, "weight_hh_l0", axs[1])
+    
+    weights = model.fc.state_dict()["weight"].detach().cpu().numpy()
+    plot_svd(weights, "weight_out", axs[2])
+
+    fig.savefig('weights.png')
