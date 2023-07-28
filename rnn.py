685f2c6c15fa02da123929b8050d4ab7203e7a60
vboboeva <vezha.boboeva@gmail.com>
2023-07-28 19:28:32 +0100

    Put some parts into functions for easier readability and implemented copies of some sequences in training data. Still network doesn't learn, maybe there is a bug.

--- rnn.py
+++ rnn.py
@@ -14,38 +14,24 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 from tqdm import tqdm
 import string
+import random
 from train import train
 from train import predict
 from model import RNN
 
+# count how many transitions of each kind we have
+def count(M):    
+    count=np.zeros((alpha, alpha))
+    for i in range(len(M)):
+        for j in range(L-1):
+            x = letter_to_index[M[i,j]]
+            y = letter_to_index[M[i,j+1]]
+            count[x,y]+=1
+    count /= np.sum(count, axis=1)[:, None]
+    return count
 
-if __name__ == "__main__":
-
-    # sequence parameters 
-    L=5
-    m=2
-
-    # network parameters
-    n_hidden = 100
-    n_layers = 1
-
-    # training
-
-    whichloss='CE'
-    n_epochs = 200
-    batch_size = 5
-    learning_rate = 0.001
-    frac_train=.8
-
-    # load the number of inputs
-    alpha = len(loadtxt('input/alphabet.txt', dtype='str'))
-
-    print(alpha)
-
-    # number of initial letters to cue net with
-    start=4
-
-    # make a dictionary
+# make a dictionary
+def make_dicts(alpha):
     letter_to_index = {}
     keys = list(string.ascii_lowercase)[:alpha]
     values = np.arange(alpha)
@@ -58,17 +44,9 @@ if __name__ == "__main__":
     for i, k in enumerate(keys):
         index_to_letter[k] = values[i]
 
-    print(index_to_letter)
+    return letter_to_index, index_to_letter
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("device =", device)
-
-    # input_num_units, hidden_num_units, num_layers, output_num_units
-    model = RNN(alpha, n_hidden, n_layers, alpha, device=device)
-
-    # load types
-    types = np.array(loadtxt('input/structures_L%d_m%d.txt'%(L, m), dtype='str')).reshape(-1)
-
+def load_tokens(types):
     all_tokens=[]
     # load all the tokens corresponding to that type
     for t, type_ in enumerate(types[:1]):
@@ -77,15 +55,6 @@ if __name__ == "__main__":
         tokens_arr = np.vstack([np.array(list(token_)) for token_ in tokens])
         all_tokens.append(tokens_arr)
     all_tokens = np.vstack(all_tokens)
-    
-    # count how many transitions of each kind we have
-    count=np.zeros((alpha, alpha))
-    for i in range(len(all_tokens)):
-        for j in range(L-1):
-            x = letter_to_index[all_tokens[i,j]]
-            y = letter_to_index[all_tokens[i,j+1]]
-            count[x,y]+=1
-    count /= np.sum(count, axis=1)[:, None]
 
     # turn letters into one hot vectors
     x = torch.zeros((L, len(all_tokens), alpha), dtype=torch.float32)
@@ -96,41 +65,108 @@ if __name__ == "__main__":
             pos = np.append(pos, letter_to_index[letter])
         x[:,i,:] = F.one_hot(torch.tensor(pos.astype(int)), alpha)
 
-    '''
-    make train and test data
+    return x, all_tokens
 
-    '''    
+
+
+if __name__ == "__main__":
+
+    # sequence parameters 
+    L=3
+    m=2
+
+    # network parameters
+    n_hidden = 300
+    n_layers = 1
+
+    # training
+    whichloss='CE'
+    n_epochs = 500
+    batch_size = 10
+    learning_rate = 0.0001
+    frac_train=.8
+    start=2     # number of initial letters to cue net with
+    repeats=10
+
+    # load the number of inputs
+    alpha = len(loadtxt('input/alphabet.txt', dtype='str'))
+    print(alpha)
+
+    letter_to_index, index_to_letter = make_dicts(alpha)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("device =", device)
+
+    # input_num_units, hidden_num_units, num_layers, output_num_units
+    model = RNN(alpha, n_hidden, n_layers, alpha, device=device)
+
+    # load types
+    types = np.array(loadtxt('input/structures_L%d_m%d.txt'%(L, m), dtype='str')).reshape(-1)
+
+    x, all_tokens = load_tokens(types)
+
+    # make train and test data
+
     n_train = int(frac_train*len(all_tokens))
     n_test = len(all_tokens) - n_train
 
-    print(f"{n_train} training sequences")
-    print(f"{n_test} testing sequences")
 
     # torch.manual_seed(0)
-    # print("seed", torch.manual_seed(0))
     ids = torch.randperm(len(all_tokens))
-    # print('ids', ids)
     train_ids = ids[:n_train]
     test_ids = ids[n_train:]
+    X_train = x[:,train_ids,:]
+    X_test = x[:,test_ids,:]
+
+    print('len train', np.shape(X_train))
 
     tokens_train=all_tokens[train_ids,:]
     tokens_test=all_tokens[test_ids,:]
 
-    '''
-    train and test network
+    # repeat some training data 
+    tokens_train_repeated=[]
+    train_ids_repeated = []
+    X_train_repeated=[]
+    
+    for i in range(len(tokens_train)):
+        id_=train_ids[i]
+        random_number = random.randint(1, repeats)
+        # print(i, np.shape(X_train[:,i,:]), random_number)
+        
+        X_tostack = (np.repeat(X_train[:, i, :, np.newaxis], random_number, axis=2)).permute(0,2,1)
+        
+        if i == 0:
+            tokens_train_repeated = np.tile(tokens_train[i], (random_number,1))
+            X_train_repeated = X_tostack
+            # print(np.shape(X_train_repeated))
+        else:
+            tokens_train_repeated = np.vstack((tokens_train_repeated, np.tile(tokens_train[i], (random_number,1))))
+            
+            X_train_repeated = np.concatenate((X_train_repeated, X_tostack), axis = 1)
+        
+        train_ids_repeated = np.append(train_ids_repeated, np.arange(id_, id_+random_number,1))
 
-    '''
+
+    count=count(tokens_train_repeated)
+    train_ids_repeated=torch.tensor(train_ids_repeated.astype(int))
+    X_train_repeated=torch.tensor(X_train_repeated)
+    
+
+
+    # train and test network
 
     n_batches = len(train_ids)//batch_size
     optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0)
 
     ###################################################
+    print('train_ids', train_ids)
+    print('train_ids_repeated', len(train_ids_repeated))
+    print('train_ids_repeated', np.shape(X_train_repeated))
     
-    train_losses, test_losses, train_accuracies, test_accuracies = train(x, train_ids, test_ids, tokens_train, tokens_test, model, optimizer, whichloss, L, n_epochs, n_batches, batch_size, alpha, letter_to_index, index_to_letter, start)
+    train_losses, test_losses, train_accuracies, test_accuracies = train(X_train_repeated, X_test, train_ids_repeated, test_ids, tokens_train_repeated, tokens_test, model, optimizer, whichloss, L, n_epochs, n_batches, batch_size, alpha, letter_to_index, index_to_letter, start)
 
     ###################################################
     # X_train:  L x len(trainingdata) x alpha
-
 
     Wio=np.dot( model.fc.state_dict()["weight"].detach().cpu().numpy(),
          model.rnn.state_dict()["weight_ih_l0"].detach().cpu().numpy() )
