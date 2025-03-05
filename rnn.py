93a300d79dd183fb8f453e0b01b5e59dffcb520f
vboboeva <vezha.boboeva@gmail.com>
2025-03-05 11:50:48 +0000

    [rnn] running simulation for every combination of n-tuple classes

--- rnn.py
+++ rnn.py
@@ -6,21 +6,21 @@ import torch.nn as nn
 import torch.optim as optim
 import string
 from functools import partial
+import itertools
+import pickle
 from functions import * 
 from train import train
 from model import RNN, RNNAutoencoder, RNNMulti, LinearWeightDropout
-
 
 ###########################################
 ################## M A I N ################
 ###########################################
 
 def main(
-	learning_rate, n_hidden, sim, sim_datasplit,
+	n_types, n_hidden, sim, sim_datasplit,
 	# network parameters
 	n_layers=1,
 	n_latent=7,
-	# L = 4,
 	m = 2,
 	task=None,
 	objective='CE',
@@ -28,12 +28,12 @@ def main(
 	from_file = [], 
 	to_freeze = [], 
 	init_weights=None, 
+	learning_rate = 0.001,
 	transfer_func='relu',
 	n_epochs=10,
 	batch_size=7,
 	frac_train=0.7,  
 	n_repeats=1,  
-	n_types=-1,  
 	alpha=5,
 	snap_freq=2,
 	drop_connect = 0.,
@@ -53,95 +53,137 @@ def main(
 
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-	X_train, X_test, y_train, y_test, tokens_train, tokens_test, labels_train, labels_test, num_classes = make_tokens(data_balance, alpha, L, m, n_types, frac_train, letter_to_index, sim_datasplit)
+	types = np.array(loadtxt('input/structures_L%d_m%d.txt'%(cue_size, m), dtype='str')).reshape(-1)
 
-	# Train and test network
-	torch.manual_seed(sim)
-	n_batches = len(tokens_train) // batch_size
+	# load all the tokens corresponding to that type
+	if len(types) < n_types:
+		raise ValueError('Not enough types! Please adjust cue_size.')
 
-	# n_epochs for which take a snapshot of neural activity
-	epochs_snapshot = np.arange(0, int(n_epochs)+1, snap_freq)
+	torch.manual_seed(sim_datasplit)
+	types_suffix = generate_random_strings(len(types), L, sim_datasplit) 
+	combined = [s1 + s2 for s1, s2 in zip(types, types_suffix)]
+	types = combined
 
-	if drop_connect != 0.:
-		layer_type = partial(LinearWeightDropout, drop_p=drop_connect)
+	if n_types > 0:
+			# Get all ntype-element combinations
+			type_combinations = list(itertools.combinations(types, n_types))
+
+	# if the number of possible combinations is too large, we just consider the first 100
+	if len(type_combinations) > 25:
+		random.shuffle(type_combinations)
+		type_combinations = type_combinations[:25]
 	else:
-		layer_type = nn.Linear
+		pass
 
-	# Create the model
-	if task in ['RNNClass', 'RNNPred']:
-		if task == 'RNNClass':
-			output_size = num_classes
+	with open('%s/classes.pkl'% (output_folder_name), 'wb') as handle:
+		pickle.dump(list(type_combinations), handle, protocol=pickle.HIGHEST_PROTOCOL)
+	
+	print(f'number of {n_types}-tuple combinations)', len(type_combinations))
+
+	for t, types_chosen in enumerate(list(type_combinations)):
+
+		print('types_chosen', types_chosen)
+
+		X_train, X_test, y_train, y_test, tokens_train, tokens_test, labels_train, labels_test, num_classes = make_tokens(types_chosen, data_balance, alpha, cue_size, L, m, n_types, frac_train, letter_to_index)
+
+		# Train and test network
+		torch.manual_seed(sim)
+		n_batches = len(tokens_train) // batch_size
+
+		# n_epochs for which take a snapshot of neural activity
+		epochs_snapshot = np.arange(0, int(n_epochs)+1, snap_freq)
+
+		if drop_connect != 0.:
+			layer_type = partial(LinearWeightDropout, drop_p=drop_connect)
 		else:
-			output_size = alpha
-		model = RNN(alpha, n_hidden, n_layers, output_size, 
-		nonlinearity=transfer_func, device=device, 
-		model_filename=model_filename, from_file=from_file,
-		to_freeze=to_freeze, init_weights=init_weights, layer_type=layer_type)
-	
-	elif task == 'RNNAuto':
-		model = RNNAutoencoder(alpha, n_hidden, n_layers, n_latent, L, device=device)
-	
-	elif task == 'RNNMulti':
-		model = RNNMulti(alpha, n_hidden, n_layers, n_latent, num_classes, L, device=device, model_filename=model_filename, from_file=from_file, to_freeze=to_freeze, init_weights=init_weights, layer_type=layer_type)
+			layer_type = nn.Linear
 
-	else:
-		raise ValueError(f"Model not recognized: {task}")
+		# Create the model
+		if task in ['RNNClass', 'RNNPred']:
+			if task == 'RNNClass':
+				output_size = num_classes
+			else:
+				output_size = alpha
+			model = RNN(alpha, n_hidden, n_layers, output_size, 
+			nonlinearity=transfer_func, device=device, 
+			model_filename=model_filename, from_file=from_file,
+			to_freeze=to_freeze, init_weights=init_weights, layer_type=layer_type)
+		
+		elif task == 'RNNAuto':
+			model = RNNAutoencoder(alpha, n_hidden, n_layers, n_latent, L, device=device)
+		
+		elif task == 'RNNMulti':
+			model = RNNMulti(alpha, n_hidden, n_layers, n_latent, num_classes, L, device=device, model_filename=model_filename, from_file=from_file, to_freeze=to_freeze, init_weights=init_weights, layer_type=layer_type)
 
-	# Set up the optimizer
-	optimizer = optim.Adam(
-			model.parameters(),
-			lr=learning_rate, weight_decay=0.) # Putting weight_decay nonzero here will apply it to all the weights in the model, not what we want
+		else:
+			raise ValueError(f"Model not recognized: {task}")
 
-	if task != 'RNNMulti':
-		test_tasks = [task]
-		results, token_to_type, token_to_set = make_results_dict(test_tasks[0], tokens_train, tokens_test, labels_train, labels_test, ablate, epochs_snapshot)
-		results_list = [results]
-	else:
-		test_tasks = ['RNNClass', 'RNNPred', 'RNNAuto']
-		results_list = []
-		for test_task in test_tasks:
-			results, token_to_type, token_to_set = make_results_dict(test_task, tokens_train, tokens_test, labels_train, labels_test, ablate, epochs_snapshot)
-			results_list.append(results)
+		# Set up the optimizer
+		optimizer = optim.Adam(
+				model.parameters(),
+				lr = learning_rate, weight_decay=0.) # Putting weight_decay nonzero here will apply it to all the weights in the model, not what we want
 
-	print('TRAINING NETWORK')
+		if task != 'RNNMulti':
+			test_tasks = [task]
+			results, token_to_type, token_to_set = make_results_dict(test_tasks[0], tokens_train, tokens_test, labels_train, labels_test, ablate, epochs_snapshot)
+			results_list = [results]
+		else:
+			test_tasks = ['RNNClass', 'RNNPred', 'RNNAuto']
+			results_list = []
+			for test_task in test_tasks:
+				results, token_to_type, token_to_set = make_results_dict(test_task, tokens_train, tokens_test, labels_train, labels_test, ablate, epochs_snapshot)
+				results_list.append(results)
 
-	for epoch in range(n_epochs + 1):
-		if epoch in epochs_snapshot:
-			for test_task, results in zip(test_tasks, results_list):
-				test(results, model, X_train, X_test, y_train, y_test, tokens_train, tokens_test, labels_train, labels_test, letter_to_index, index_to_letter, test_task, objective, n_hidden, L, alphabet, ablate, delay, epoch, cue_size)
+		print('TRAINING NETWORK')
 
-		train(X_train, y_train, model, optimizer, objective, L, n_batches, batch_size, alphabet, letter_to_index, index_to_letter,  task=task, weight_decay=weight_decay, delay=delay, teacher_forcing_ratio=teacher_forcing_ratio)
+		for epoch in range(n_epochs + 1):
+			if epoch in epochs_snapshot:
+				for test_task, results in zip(test_tasks, results_list):
+					test(results, model, X_train, X_test, y_train, y_test, tokens_train, tokens_test, labels_train, labels_test, letter_to_index, index_to_letter, test_task, objective, n_hidden, L, alphabet, ablate, delay, epoch, cue_size)
 
-		# Print loss
-		if epoch in epochs_snapshot:
-			print(f'Epoch {epoch}', end='   ')
-			for test_task, results in zip(test_tasks, results_list):
-				if test_task == 'RNNClass' or test_task == 'RNNAuto':
-					meanval_train=np.mean([results['Loss'][k][epoch][0] for k in results['Loss'].keys() if token_to_set[k] == 'train'])
-					meanval_test=np.mean([results['Loss'][k][epoch][0] for k in results['Loss'].keys() if token_to_set[k] == 'test'])
-					print(f'{test_task} Loss Tr {meanval_train:.2f} Loss Test {meanval_test:.2f}', end='   ')
-		   
-				elif test_task == 'RNNPred':
-					losses = results['Loss'][epoch][0]
-					predicted_tokens = results['Retrieval'][epoch][0]
+			train(X_train, y_train, model, optimizer, objective, L, n_batches, batch_size, alphabet, letter_to_index, index_to_letter,  task=task, weight_decay=weight_decay, delay=delay, teacher_forcing_ratio=teacher_forcing_ratio)
 
-					print_retrieval_color(test_task, losses, predicted_tokens, tokens_train, tokens_test)
-			print('\n')
-						
-	return model, results_list, test_tasks, token_to_type, token_to_set
+			# Print loss
+			if epoch in epochs_snapshot:
+				print(f'Epoch {epoch}', end='   ')
+				for test_task, results in zip(test_tasks, results_list):
+					if test_task == 'RNNClass' or test_task == 'RNNAuto':
+						meanval_train=np.mean([results['Loss'][k][epoch][0] for k in results['Loss'].keys() if token_to_set[k] == 'train'])
+						meanval_test=np.mean([results['Loss'][k][epoch][0] for k in results['Loss'].keys() if token_to_set[k] == 'test'])
+						print(f'{test_task} Loss Tr {meanval_train:.2f} Loss Test {meanval_test:.2f}', end='   ')
+			
+					elif test_task == 'RNNPred':
+						losses = results['Loss'][epoch][0]
+						predicted_tokens = results['Retrieval'][epoch][0]
+
+						# print_retrieval_color(test_task, losses, predicted_tokens, tokens_train, tokens_test)
+				# print('\n')
+							
+		print('SAVING RESULTS')
+		# Save the model state
+		# torch.save(model.state_dict(), '%s/model_state_sim%d.pth' % (output_folder_name, sim))
+
+		for results, task in zip(results_list, test_tasks):
+			with open('%s/results_task%s_sim%d_classcomb%d.pkl'% (output_folder_name, task, sim, t), 'wb') as handle:
+				pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+		with open('%s/token_to_set_classcomb%d.pkl'% (output_folder_name, t), 'wb') as handle:
+			pickle.dump(token_to_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+		with open('%s/token_to_type_classcomb%d.pkl'% (output_folder_name, t), 'wb') as handle:
+			pickle.dump(token_to_type, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 ##################################################
 
 if __name__ == "__main__":
 
 	# params = loadtxt('params_L4_m2.txt')
-	params = loadtxt("params.txt")
+	params = loadtxt("params_new.txt")
 
 	main_kwargs = dict(
 		# network parameters
 		n_layers = 1, # number of RNN layers
 		n_latent = 10, # size of latent layer (autoencoder only!!)
-		# L = 4, # length of sequence
 		m = 2, # number of unique letters in each sequence
 		task = 'RNNPred',  # choose btw 'RNNPred', 'RNNClass', RNNAuto', or 'RNNMulti' 
 		objective = 'CE', # choose btw cross entr (CE) and mean sq error (MSE)
@@ -149,19 +191,19 @@ if __name__ == "__main__":
 		from_file = [], # choose one or more of ['i2h', 'h2h'], if setting state of layers from file
 		to_freeze = [], # choose one or more of ['i2h','h2h'], those  layers not to be updated   
 		init_weights = None, # choose btw None, 'const', 'lazy', 'rich' , weight initialization
+		learning_rate = 0.001,
 		transfer_func = 'relu', # transfer function of RNN units only
-		n_epochs = 300, # number of training epochs
+		n_epochs = 100, # number of training epochs
 		batch_size = 1, #16, # GD if = size(training set), SGD if = 1
 		frac_train = 110./140., # fraction of dataset to train on
 		n_repeats = 1, # number of repeats of each sequence for training
-		n_types = 1, # # number of types to train net with: 1 takes just the first, -1 takes all types. Set minimum 2 for class task to make sense
-		alpha = 10, # size of alphabet
+		alpha = 15, # size of alphabet
 		snap_freq = 5, # snapshot of net activity every snap_freq epochs
 		drop_connect = 0., # fraction of dropped connections (reg)
 		# weight_decay = 0.2, # weight of L1 regularisation
 		ablate = False, # whether to test net with ablated units
 		delay = 0, # number of zero-padding steps at end of input
-		cue_size = 2, # number of letters to cue net with (prediction task only!!)
+		cue_size = 5, # number of letters to cue net with (prediction task only!!)
 		data_balance = 'class', # choose btw 'class' and 'whatwhere'
 		teacher_forcing_ratio = 1.  # Add teacher forcing ratio parameter
 	)
@@ -174,7 +216,7 @@ if __name__ == "__main__":
 		transfer=True
 
 	L_col_index = 0
-	lr_col_index = 1
+	n_types_col_index = 1
 	n_hidden_col_index = 2
 	sim_datasplit_col_index = 3
 	sim_col_index = 4
@@ -182,21 +224,18 @@ if __name__ == "__main__":
 
 	# size is the number of serial simulations running on a single node of the cluster, set this accordingly with the number of arrays in order to cover all parameters in the parameters.txt file
 
-	size = 25
+	size = 9
 	for i in range(size):
 		row_index = index * size + i
 
 		L = int(params[row_index, L_col_index])
-		learning_rate = params[row_index, lr_col_index]
+		n_types = 8#int(params[row_index, n_types_col_index])
 		n_hidden = int(params[row_index, n_hidden_col_index])
 		sim_datasplit = int(params[row_index, sim_datasplit_col_index])
 		sim = int(params[row_index, sim_col_index])
 
-		output_folder_name = 'Task%s_N%d_nlatent%d_L%d_m%d_alpha%d_nepochs%d_lr%.5f_bs%d_ntypes%d_fractrain%.1f_obj%s_init%s_transfer%s_cuesize%d_delay%d_datasplit%s' % ( main_kwargs['task'], n_hidden, main_kwargs['n_latent'], L, main_kwargs['m'], main_kwargs['alpha'], main_kwargs['n_epochs'], learning_rate, main_kwargs['batch_size'], main_kwargs['n_types'], main_kwargs['frac_train'], main_kwargs['objective'], main_kwargs['init_weights'],  main_kwargs['transfer_func'], main_kwargs['cue_size'], main_kwargs['delay'], sim_datasplit )
+		output_folder_name = 'Task%s_N%d_nlatent%d_L%d_m%d_alpha%d_nepochs%d_lr%.5f_bs%d_ntypes%d_fractrain%.1f_obj%s_init%s_transfer%s_cuesize%d_delay%d_datasplit%s' % ( main_kwargs['task'], n_hidden, main_kwargs['n_latent'], L, main_kwargs['m'], main_kwargs['alpha'], main_kwargs['n_epochs'], main_kwargs['learning_rate'], main_kwargs['batch_size'], n_types, main_kwargs['frac_train'], main_kwargs['objective'], main_kwargs['init_weights'],  main_kwargs['transfer_func'], main_kwargs['cue_size'], main_kwargs['delay'], sim_datasplit )
 
 		os.makedirs(output_folder_name, exist_ok=True)
 
-		model, results_list, test_tasks, token_to_type, token_to_set = main(learning_rate, n_hidden, sim, sim_datasplit, **main_kwargs)
-
-		print('SAVING FILES')
-		savefiles(output_folder_name, sim, model, results_list, test_tasks, token_to_type, token_to_set)
+		main(n_types, n_hidden, sim, sim_datasplit, **main_kwargs)
