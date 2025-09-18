63cc0ffb758de7c5f722f1929a8ebb8f549a234e
vboboeva <vezha.boboeva@gmail.com>
2025-09-18 17:05:31 +0100

    [data_utils] extended to torch.Tensor

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
+import torch
 from scipy.stats import special_ortho_group
-
+from typing import Union
 
 def generate_random_rotations (N: int, M: int = 1) -> np.ndarray:
 
@@ -13,14 +14,14 @@ def generate_random_rotations (N: int, M: int = 1) -> np.ndarray:
 	return R
 
 
-def augment_data (X: np.ndarray, M: int, flatten: bool = True) -> np.ndarray:
+def augment_data (X: Union[np.ndarray,torch.Tensor], M: int, flatten: bool = True) -> Union[np.ndarray, torch.Tensor]:
 	'''
 	Create copies of each point in the input data array.
 
 	Arguments
 	---------
 
-	X: (..., D) np.ndarray
+	X: (..., D) np.ndarray or torch.Tensor
 		Input data
 
 	M: int
@@ -29,17 +30,26 @@ def augment_data (X: np.ndarray, M: int, flatten: bool = True) -> np.ndarray:
 	Returns
 	-------
 
-	Y: (..., M, D)
+	Y: (..., M, D) np.ndarray or torch.Tensor
 		Augmented data
 	'''
 
+	if not len(X):
+		return X
+
 	D = X.shape[-1]
-
 	Rs = generate_random_rotations(D, M)
-	Y = np.einsum('...i,jki->j...k', X, Rs)
 
-	if flatten:
-		Y = np.concatenate(Y, axis=0)
+	if isinstance(X, np.ndarray):
+		Y = np.einsum('...i,jki->j...k', X, Rs)
+		if flatten:
+			Y = np.concatenate(Y, axis=0)
+
+	elif isinstance(X, torch.Tensor):
+		Rs = torch.tensor(Rs, dtype=torch.float)
+		Y = torch.einsum('...i,jki->j...k', X, Rs)
+		if flatten:
+			Y = np.cat(Y, dim=0)
 
 	return Y
 
